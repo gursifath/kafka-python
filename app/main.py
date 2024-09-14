371e5951c5958c64bb18bf380ee1e1690042c83a
Gursifath Bhasin <gursifath@users.noreply.github.com>
2024-09-14 19:01:15 -0400

    parse correlation ID

--- app/main.py
+++ app/main.py
@@ -1,24 +1,29 @@
 import socket  # noqa: F401
 
+# data = datab'\x00\x00\x00#\x00\x12\x00\x046\xbd\x82c\x00\tkafka-cli\x00\nkafka-cli\x040.1\x00'
+
 class Broker:
     def __init__(self, port):
         self.server = socket.create_server(("localhost", port), reuse_port=True)
+        self.client = None
+        self.addr = None
 
-    def construct_response(self):
-        header = (7).to_bytes(length=4, byteorder="big", signed=True)
+    def construct_response(self, data):
+        correlation_id = int.from_bytes(data[8:12], byteorder="big")
+        header = correlation_id.to_bytes(length=4, byteorder="big", signed=True)
         message_length = (len(header)).to_bytes(length=4, byteorder="big", signed=True)
         response = message_length + header
         return response
     
-    def handle_request(self, client):
-        client.recv(1024)
-        client.sendall(self.construct_response())
-        client.close()
+    def handle_request(self):
+        data = self.client.recv(1024)
+        self.client.sendall(self.construct_response(data))
+        self.client.close()
 
     def stream(self):
         while True:
-            client, addr = self.server.accept()
-            self.handle_request(client)
+            self.client, self.addr = self.server.accept()
+            self.handle_request()
 
 def main():
     # You can use print statements as follows for debugging,
@@ -30,4 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
